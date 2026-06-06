dc5103ce2e246b4ed3ab3d2c55da112462cfe0bc
Manas Rajdeep <manasrajdeep@outlook.com>
2026-06-06 09:07:58 +0530

    cart reprository

--- src/features/cart/cart.controller.js
+++ src/features/cart/cart.controller.js
@@ -1,29 +1,67 @@
+import { ObjectId } from "mongodb";
 import cartItemModel from "./cart.model.js";
+import CartItemRepository from "./cart.repository.js";
 
 export class cartItemController{
-    add(req,res){
-        const {product, quantity} =req.query;
+    constructor(){
+        this.cartItemRepository=new CartItemRepository();
+    }
+
+    //from card repository
+   async add(req,res){
+        const {product, quantity} =req.body;
         const userId=req.userID;
-        console.log(userId);
-        cartItemModel.add(product,userId,quantity);
+        console.log(quantity)
+        await this.cartItemRepository.add(product,userId,quantity);
         res.status(201).send("Cart is Updated");
     }
 
-    get(req,res){ 
-        const userID=req.userID;
-        const cartItems=cartItemModel.get(userID);
-        console.log(cartItems);
-        console.log(userID);
-        return res.status(200).send(cartItems);
 
+    // //this is for card model
+    // add(req,res){
+    //     const {product, quantity} =req.query;
+    //     const userId=req.userID;
+    //     console.log(userId);
+    //     cartItemModel.add(product,userId,quantity);
+    //     res.status(201).send("Cart is Updated");
+    // }
+
+    //this is for card Repository
+    async get(req,res){ 
+        const userID=req.userID;
+        const cartItems=await this.cartItemRepository.get(userID);
+        return res.status(200).send(cartItems);
     }
-    delete(req,res){
+
+    // //this is for card model
+    // get(req,res){ 
+    //     const userID=req.userID;
+    //     const cartItems=cartItemModel.get(userID);
+    //     console.log(cartItems);
+    //     console.log(userID);
+    //     return res.status(200).send(cartItems);
+
+    // }
+
+    //this is from card repository
+    async delete(req,res){
         const userId=req.userID;
         const cartItemId=req.params.id;
-        const error=cartItemModel.delete(cartItemId,userId);
-        if(error){
-            return res.status(404).send(error);
+        const isDeleted=this.cartItemRepository.delete(userId,cartItemId);
+        if(!isDeleted){
+            return res.status(404).send("Item not found");
         }
        return res.status(200).send("Deleted successfuly")
     }
+
+    // //this is for card model
+    // delete(req,res){
+    //     const userId=req.userID;
+    //     const cartItemId=req.params.id;
+    //     const error=cartItemModel.delete(cartItemId,userId);
+    //     if(error){
+    //         return res.status(404).send(error);
+    //     }
+    //    return res.status(200).send("Deleted successfuly")
+    // }
 }
---
+++ src/features/cart/cart.repository.js
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb";
+import { getDB } from "../../config/mongodb.js"
+
+export default class CartItemRepository{
+    constructor() {this.collection="cartItems"};
+
+    async add(product,userId,quantity){
+        const db=getDB();
+        const collection=db.collection(this.collection);
+        const newItems=await collection.insertOne({product:new ObjectId(product),userId:new ObjectId(userId),quantity})
+
+    }
+
+   async get(userID){
+    const db=getDB();
+    const collection=db.collection(this.collection);
+    const getItems=await collection.find({userId:new ObjectId(userID)}).toArray();
+    return getItems
+
+   }
+
+   async delete(userID,cartItemId){
+    const db=getDB();
+    const collection=db.collection(this.collection);
+    const deleteItems=await collection.deleteOne({_id:new ObjectId(cartItemId),userID:new ObjectId(userID)});
+    return deleteItems.deletedCount>0;
+   }
+}
