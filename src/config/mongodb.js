138258546f39888ae1cc55b9f8789efb9f9c9c28
Manas Rajdeep <manasrajdeep@outlook.com>
2026-06-09 08:10:03 +0530

    modifying_id

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -7,6 +7,7 @@ export const connectToMongoDB=()=>{
     MongoClient.connect(process.env.DB_URL).then((clientInstance)=>{
         client=clientInstance;
         console.log("Connected to Mongodb");
+        createCounter(client.db());
     }).catch(err=>{
         console.log(err);
     })
@@ -16,4 +17,11 @@ export const connectToMongoDB=()=>{
 export const getDB=()=>
 {
     return client.db();
+}
+
+const createCounter=async(db)=>{
+    const existingCounter=await db.collection("counters").findOne({_id:'cartItemId'});
+    if(!existingCounter){
+        await db.collection("counters").insertOne({_id:'cartItemId',value:0});
+    }
 }
--- src/features/cart/cart.repository.js
+++ src/features/cart/cart.repository.js
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, ReturnDocument } from "mongodb";
 import { getDB } from "../../config/mongodb.js"
 
 export default class CartItemRepository{
@@ -7,12 +7,15 @@ export default class CartItemRepository{
     async add(product,userId,quantity){
         const db=getDB();
         const collection=db.collection(this.collection);
+        const id=await this.getNextCounter(db);
 
         //find the document
         //either insert or update
         await collection.updateOne(
             {product:new ObjectId(product),userId:new ObjectId(userId),quantity},
-            {$inc:{
+            {   $setOnInsert:{
+                _id:id},
+                $inc:{
                 quantity:quantity
             }},
             {upsert:true}
@@ -36,5 +39,20 @@ export default class CartItemRepository{
     const collection=db.collection(this.collection);
     const deleteItems=await collection.deleteOne({_id:new ObjectId(cartItemId),userID:new ObjectId(userID)});
     return deleteItems.deletedCount>0;
+   }
+   async getNextCounter(db){
+    const resultDocument=await db.collection("counters").findOneAndUpdate(
+        {
+            _id:'cartItemId'
+        },
+        {
+            $inc:{value:1}
+        },
+        {returnDocument:'after'}
+    )
+    console.log(resultDocument)
+    console.log(resultDocument.value)
+
+    return resultDocument.value;
    }
 }
