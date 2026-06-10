1b8ba35acddc512a493cc1556673f9b9127cf96a
Manas Rajdeep <manasrajdeep@outlook.com>
2026-06-10 11:33:02 +0530

    #

--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import { getDB } from "../../config/mongodb.js"
-
+import OrderModel from "./order.model.js";
 
 export default class OrderRepository{
 
@@ -9,18 +9,37 @@ export default class OrderRepository{
     }
 
     async placeOrder(userId){
-        
+       try{ 
+        const db=getDB();
+
         //1.Get cartItems and calculate total amount.
-          await this.getTotalAmount(userId);
-
-
-        //2.Create an order record
+        const items = await this.getTotalAmount(userId);
+        const finaltotalAmount=items.reduce((acc,item)=>acc+item.totalAmount,0);
+        console.log(finaltotalAmount);
+       
+        //2.Create an order record.
+        const newOrder = new OrderModel(new ObjectId(userId),finaltotalAmount,new Date());
+        db.collection(this.collection).insertOne(newOrder);
+        
 
         //3.Reduce the stock.
+        for(let item of items){
+            await db.collection("products").updateOne(
+                {_id:item.productID},
+                {$inc:{stock:-item.quantity}}
+            )
+        }
+        throw new Error("Something is wrong i placeOrder");
+        
+        //4 Clear the cart Items.
+        await db.collection("cartItems").deleteMany({
+            userId: new ObjectId(userId)
+        });
+        return;
 
-        //4 Clear the cart Items
-
-
+       }catch(err){
+        console.log(err);
+       }
     }
     async getTotalAmount(userId){
         const db=getDB();
@@ -52,8 +71,6 @@ export default class OrderRepository{
                 }
             }
         ]).toArray();
-       const finaltotalAmount=items.reduce((acc,item)=>acc+item.totalAmount,0);
-       console.log(finaltotalAmount);
-       console.log(items);
+        return items;
     }
-}
+    }
